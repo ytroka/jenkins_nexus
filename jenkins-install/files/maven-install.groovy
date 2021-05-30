0c79240866d69a0e04740f1e72048be711bb12bf
Andrey Arkhipov <piratborman@gmail.com>
2021-05-30 21:30:04 +0300

    add plugins and maven to jenkins

---
+++ jenkins-install/files/maven-install.groovy
@@ -0,0 +1,32 @@
+import jenkins.*;
+import jenkins.model.*;
+import hudson.*;
+import hudson.model.*;
+
+mavenName = "maven"
+mavenVersion = "3.8.1"
+println("Checking Maven installations...")
+
+// Grab the Maven "task" (which is the plugin handle).
+mavenPlugin = Jenkins.instance.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0]
+
+// Check for a matching installation.
+maven3Install = mavenPlugin.installations.find {
+   install -> install.name.equals(mavenName)
+}
+
+// If no match was found, add an installation.
+if(maven3Install == null) {
+   println("No Maven install found. Adding...")
+
+   newMavenInstall = new hudson.tasks.Maven.MavenInstallation('maven', null,
+    [new hudson.tools.InstallSourceProperty([new hudson.tasks.Maven.MavenInstaller(mavenVersion)])]
+)
+
+   mavenPlugin.installations += newMavenInstall
+   mavenPlugin.save()
+
+   println("Maven install added.")
+} else {
+   println("Maven install found. Done.")
+}
